8aa24ef5e92438bc09ba797a591f8f423cfcf78e
kotaesaki <gurico1229@gmail.com>
2020-12-10 07:36:37 +0900

    hoge

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 pprint.pprint(sys.path)
-tagger = MeCab.Tagger ("mecabrc")
+tagger = MeCab.Tagger ("-Owakati")
 print(tagger.parse("今日はいい天気ですね。"))
 
 @app.route("/callback", methods=['POST'])
