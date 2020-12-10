8c7b091c22e58874b1832ba00dbdafa00abd4d01
kotaesaki <gurico1229@gmail.com>
2020-12-10 09:08:42 +0900

    aiu

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 pprint.pprint(sys.path)
+tagger = MeCab.Tagger()
 
 @app.route("/callback", methods=['POST'])
 def callback():
